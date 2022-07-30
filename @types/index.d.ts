e8692071452c7a72c19a8ad4e07d4beabe8b6cfe
fcastrocs <44808362+fcastrocs@users.noreply.github.com>
2022-07-29 21:39:33 -0400

    make it less opinionated

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,8 +3,16 @@ import { NextFunction as Next, Request as Req, Response as Res } from "express";
 
 export default function requestLimiter(options: Options): (req: Req, res: Res, next: Next) => Promise<void>;
 
+export interface CompoundKey {
+  userId: 1;
+  path: 1;
+  [key: string]: number;
+}
+
 export interface Options {
   client: MongoClient;
+  collectionName: string;
+  customField?: { name: string; reqBodyProp: string };
   excludePaths: string[];
   expireAfterSeconds: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import { NextFunction as Next, Response as Res, Request as Req } from "express";
-import { Options } from "../@types";
+import { CompoundKey, Options } from "../@types";
 
 export default function requestLimiter(options: Options): (req: Req, res: Res, next: Next) => Promise<void> {
-  const collection = options.client.db().collection("rate-limiter");
-  collection.createIndex({ userId: 1, path: 1, steamUsername: 1 }, { unique: true });
+  const collection = options.client.db().collection(options.collectionName);
+
+  // create compound key to look for when rate limiting
+  // default is {userId: req.session.userId, path: req.path}
+  // optionally another field from req.body can be added through options.customField
+  const compoundKey: CompoundKey = { userId: 1, path: 1 };
+  if (options.customField) compoundKey[options.customField.name] = 1;
+  collection.createIndex(compoundKey, { unique: true });
   collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: options.expireAfterSeconds });
 
   return async function (req: Req, res: Res, next: Next) {
-    if (options.excludePaths.includes(req.path)) {
-      next();
-      return;
-    }
+    if (options.excludePaths.includes(req.path)) return next();
 
-    const userId = (req as any).session.userId;
-    const query = { userId, path: req.path, steamUsername: req.body.username };
+    // build query
+    const query = <any>{ userId: (req as any).session.userId, path: req.path };
+    if (options.customField) query[options.customField.name] = req.body[options.customField.reqBodyProp];
 
     if (await collection.findOne(query)) {
       res.status(403).send("You have an ongoing request for this resource.");
@@ -22,9 +26,8 @@ export default function requestLimiter(options: Options): (req: Req, res: Res, n
 
     await collection.insertOne({ ...query, createdAt: new Date() });
 
-    res.on("finish", () => {
-      collection.deleteOne(query);
-    });
+    // remove rate limit when request finishes
+    res.on("finish", () => collection.deleteOne(query));
 
     next();
   };
